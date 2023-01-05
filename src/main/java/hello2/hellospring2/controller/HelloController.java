7629e6878bbe05366ba4ccddb48e8bfac66f2a45
mwchoen <sglee@cudo.co.kr>
2023-01-05 16:01:10 +0900

    1. 스프링부트 view 환경 설정 ( 기본 )

---
+++ src/main/java/hello2/hellospring2/controller/HelloController.java
@@ -0,0 +1,21 @@
+package hello2.hellospring2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+
+        return "hello";
+        // 컨트롤러에서 리턴 값으로 문자를 반환 하면
+        // 뷰리졸버 ( viewResolver ) 가 화면을 찾아서 처리 한다
+        // 스프링 부트 템플릿엔진 기본 viewName 매핑
+        // resource:templates/ + {ViewName} + .html
+    }
+
+}
