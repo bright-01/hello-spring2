1326ec8212f0aeed0d76e24e0b0f382edbd52171
mwchoen <mwcheon@cudo.co.kr>
2023-01-10 10:47:01 +0900

    MVC와 템플릿 엔진

--- src/main/java/hello2/hellospring2/controller/HelloController.java
+++ src/main/java/hello2/hellospring2/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello2.hellospring2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -16,6 +17,12 @@ public class HelloController {
         // 뷰리졸버 ( viewResolver ) 가 화면을 찾아서 처리 한다
         // 스프링 부트 템플릿엔진 기본 viewName 매핑
         // resource:templates/ + {ViewName} + .html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
 
