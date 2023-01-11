80b9573603e2f848aa6b7c0c211680a688e98b2c
mwchoen <mwcheon@cudo.co.kr>
2023-01-11 15:18:42 +0900

    스프링 입문 - 회원 서비스 개발

---
+++ src/main/java/hello2/hellospring2/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello2.hellospring2.service;
+
+import hello2.hellospring2.domain.Member;
+import hello2.hellospring2.repository.MemberRepository;
+import hello2.hellospring2.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     * */
+    public Long join(Member member) {
+
+//           Optional<Member> result = memberRepository.findByName(member.getName()); // 이렇게 Optional 바로 꺼내서 사용하는 것을 권장 하지 않음.. 아래와 같이 사용
+//           result.ifPresent( m -> {
+//               throw new IllegalStateException("이미 존재하는 회원입니다.");
+//           });
+
+        // 그리고 아래와 같으면 메서드로 뽑는게 좋음
+        validateDuplicateMember(member);
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
