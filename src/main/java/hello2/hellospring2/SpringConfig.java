6ab42a037abf4277173c72c64dacad333a1c59c0
mwchoen <mwcheon@cudo.co.kr>
2023-01-17 15:10:25 +0900

    스프링 입문 - 순수 JDBC

--- src/main/java/hello2/hellospring2/SpringConfig.java
+++ src/main/java/hello2/hellospring2/SpringConfig.java
@@ -1,15 +1,27 @@
 package hello2.hellospring2;
 
 
+import hello2.hellospring2.repository.JdbcMemberRepository;
 import hello2.hellospring2.repository.MemberRepository;
 import hello2.hellospring2.repository.MemoryMember2Repository;
+import hello2.hellospring2.repository.MemoryMemberRepository;
 import hello2.hellospring2.service.Member2Service;
 import hello2.hellospring2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public Member2Service member2Service(){
@@ -18,6 +30,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMember2Repository();
+//        return new MemoryMember2Repository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
